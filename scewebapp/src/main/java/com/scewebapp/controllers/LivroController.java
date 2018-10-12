87d41742f2bb8e7b2866752a2bd03dfb30242edb
DV6-6170\Hp <engsoftware1@gmail.com>
2018-10-12 09:40:49 -0300

    Insere um registro no DB

--- scewebapp/src/main/java/com/scewebapp/controllers/LivroController.java
+++ scewebapp/src/main/java/com/scewebapp/controllers/LivroController.java
@@ -1,12 +1,24 @@
 package com.scewebapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.scewebapp.models.Livro;
+import com.scewebapp.repository.LivroRepository;
 
 @Controller
 public class LivroController {
-	@RequestMapping("/cadastrarLivro")
+	@Autowired
+	private LivroRepository er;
+	@RequestMapping(value="/cadastrarLivro", method=RequestMethod.GET)
 	public String form() {
 		return "livro/formLivro";
+	}
+	@RequestMapping(value="/cadastrarLivro", method=RequestMethod.POST)
+	public String form(Livro livro) {
+		er.save(livro);
+		return "redirect:/cadastrarLivro";
 	}
 }
---
+++ scewebapp/src/main/java/com/scewebapp/repository/LivroRepository.java
@@ -0,0 +1,7 @@
+package com.scewebapp.repository;
+import com.scewebapp.models.*;
+
+import org.springframework.data.repository.CrudRepository;
+public interface LivroRepository extends CrudRepository <Livro,String>{
+
+}

51be26b82ae6afed609c149141c31bf222a25089
DV6-6170\Hp <engsoftware1@gmail.com>
2018-10-12 09:25:21 -0300

    Configuração do JPA - application.properties

--- scewebapp/src/main/java/com/scewebapp/ScewebappApplication.java
+++ scewebapp/src/main/java/com/scewebapp/ScewebappApplication.java
@@ -1,9 +1,11 @@
 package com.scewebapp;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ScewebappApplication {
 
 	public static void main(String[] args) {
--- scewebapp/src/main/java/com/scewebapp/models/Livro.java
+++ scewebapp/src/main/java/com/scewebapp/models/Livro.java
@@ -1,6 +1,20 @@
 package com.scewebapp.models;
 
-public class Livro {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Livro implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	private Long id;
 	private String isbn;
 	private String titulo;
 	private String autor;

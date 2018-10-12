8344d51c1c02fe38d6339be48e64a7b02bf66def
DV6-6170\Hp <engsoftware1@gmail.com>
2018-10-12 13:35:51 -0300

    Refatoração da interface de usuario

--- scewebapp/src/main/java/com/scewebapp/controllers/LivroController.java
+++ scewebapp/src/main/java/com/scewebapp/controllers/LivroController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.scewebapp.models.Livro;
 import com.scewebapp.repository.LivroRepository;
@@ -20,5 +21,12 @@ public class LivroController {
 	public String form(Livro livro) {
 		er.save(livro);
 		return "redirect:/cadastrarLivro";
+	}
+	@RequestMapping("/livros")
+	public ModelAndView listaLivros() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Livro> livros = er.findAll();
+		mv.addObject("livros", livros);
+		return mv;
 	}
 }
